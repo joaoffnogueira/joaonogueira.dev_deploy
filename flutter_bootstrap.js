2b67f47fcbdd04f71bcc676152bfc6b7965425e7
João Nogueira <jffn@protonmail.com>
2025-08-24 10:07:26 -0300

    bump version 1.0.1

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -38,6 +38,6 @@ _flutter.buildConfig = {"engineRevision":"1e9a811bf8e70466596bcf0ea3a8b5adb5f17f
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "3579076374"
+    serviceWorkerVersion: "1443589999"
   }
 });
